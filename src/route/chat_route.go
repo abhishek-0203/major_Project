366b6861f8acc8806721dec42a6301503c2e2531
abhishek6891 <abhisheksfs6892@gmail.com>
2025-07-21 02:53:53 +0530

    Update to use *gin.RouterGroup for protected routes

--- src/route/chat_route.go
+++ src/route/chat_route.go
@@ -5,10 +5,8 @@ import (
 	"majorProject/src/chat"
 )
 
-func RegisterChat(r *gin.Engine) {
-	//func RegisterChatRoutes(router *gin.Engine) {
-	//	router.GET("/ws/chat", ChatHandler)
-	r.GET("/ws/chat", func(c *gin.Context) {
+func RegisterChat(route *gin.RouterGroup) {
+	route.GET("/ws/chat", func(c *gin.Context) {
 		chat.ChatHandler(c.Writer, c.Request)
 	})
 }
